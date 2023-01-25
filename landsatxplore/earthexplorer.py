759404b01b85646a70962b2b22108b7ea3d1b05a
HIEW Li Wen <32948315+hiewliwen@users.noreply.github.com>
2023-01-26 07:48:38 +0800

    Update earthexplorer.py
    https://github.com/yannforget/landsatxplore/issues/92

--- landsatxplore/earthexplorer.py
+++ landsatxplore/earthexplorer.py
@@ -45,6 +45,19 @@ DATA_PRODUCTS_II = {
     "sentinel_2a": "5e83a42c6eba8084",
 }
 
+DATA_PRODUCTS_III = {
+    "landsat_tm_c1": "5e83d08fd9932768",
+    "landsat_etm_c1": "5e83a507d6aaa3db",
+    "landsat_8_c1": "5e83d0b84df8d8c2",
+    "landsat_tm_c2_l1": "63231219fdd8c4e5",
+    "landsat_etm_c2_l1": "5e83d0d08fec8a66",
+    "landsat_ot_c2_l1": "5e81f14f92acf9ef",
+    "landsat_tm_c2_l2": "632312ba6c0988ef",
+    "landsat_etm_c2_l2": "632311068b0935a8",
+    "landsat_ot_c2_l2": "632210d4770592cf",
+    "sentinel_2a": "5e83a42c6eba8084",
+}
+
 def _get_tokens(body):
     """Get `csrf_token` and `__ncforminfo`."""
     csrf = re.findall(r'name="csrf" value="(.+?)"', body)[0]
@@ -159,12 +172,12 @@ class EarthExplorer(object):
         else:
             entity_id = identifier
             try:
-                url = EE_DOWNLOAD_URL.format(
-                    data_product_id=DATA_PRODUCTS[dataset], entity_id=entity_id
-                )
-                filename = self._download(url, output_dir, timeout=timeout, skip=skip)
+            url = EE_DOWNLOAD_URL.format(data_product_id=DATA_PRODUCTS[dataset], entity_id=entity_id)
+            filename = self._download(url, output_dir, timeout=timeout, skip=skip)
             except:
-                url = EE_DOWNLOAD_URL.format(
-                    data_product_id=DATA_PRODUCTS_II[dataset], entity_id=entity_id
-                )
-                filename = self._download(url, output_dir, timeout=timeout, skip=skip)
+                try:
+                    url = EE_DOWNLOAD_URL.format(data_product_id=DATA_PRODUCTS_II[dataset], entity_id=entity_id)
+                    filename = self._download(url, output_dir, timeout=timeout, skip=skip)
+                except:
+                    url = EE_DOWNLOAD_URL.format(data_product_id=DATA_PRODUCTS_III[dataset], entity_id=entity_id)
+                    filename = self._download(url, output_dir, timeout=timeout, skip=skip)
